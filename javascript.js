8a56589e5aaa85974167768f112dcc89d1db742e
Rowan Maynor <rowanmaynor@gmail.com>
2024-09-12 09:12:18 -0400

    Add a comment for when I continue later

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
 const divGridContainer = document.querySelector("#divGridContainer");
 
+// Try using loop to take in a value X, and loop X times creating X rows, and 
+// X collumns per row
 function initialSetup() {
     for(let i = 0; i < 16; i++){
         const div = document.createElement("div");

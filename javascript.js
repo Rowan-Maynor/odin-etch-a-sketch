483d45add9d14d5799dad39261f8de7836960ec5
Rowan Maynor <rowanmaynor@gmail.com>
2024-09-14 09:25:26 -0400

    Create generateGrid function
    Scrapped everything because I was over complicating it.
    stripping it back down to basics made things come together smoother.

--- javascript.js
+++ javascript.js
@@ -1,14 +1,25 @@
-const divGridContainer = document.querySelector("#divGridContainer");
+function generateGrid (gridSize) {
+    console.log("Function Ran");
+    for (let i = 0; i < gridSize; i++){
+        console.log("Creating divRow");
+        const gridRow = document.createElement("div");
+        gridRow.id = `gridRow${i}`;
+        gridRow.style.display = "flex";
+        console.log("divRow Created");
 
-// Try using loop to take in a value X, and loop X times creating X rows, and 
-// X collumns per row
-function initialSetup() {
-    for(let i = 0; i < 16; i++){
-        const div = document.createElement("div");
-        div.id = `div${i}`;
-        div.style = "flex: 1; border: solid;";
-        divGridContainer.appendChild(div);
+        for(let j = 0; j < gridSize; j++){
+            const gridCol = document.createElement("div");
+            const gridSquareSize = 100/gridSize;
+            gridCol.id = `gridCol${i}-${j}`;
+            gridCol.style.height = `${gridSquareSize}vh`;
+            gridCol.style.width = `${gridSquareSize}vh`;
+            gridCol.style.border = "solid";
+            gridRow.appendChild(gridCol);
+        }
+
+        document.body.appendChild(gridRow);
     }
+
 }
 
-initialSetup();
+generateGrid(5);
